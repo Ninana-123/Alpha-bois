856716cef6ec9800583d6d226ec5f1ba6cd2f151
Ninana-123 <sheenyeoh@gmail.com>
2023-01-25 15:49:52 +0800

    Draw shrine

---
+++ Guns of the Ronin/draw_shrine.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef Shrine_H
+#define Shrine_H
+
+#include "Graphics.h"
+#include "Physics.h"
+
+class Shrine {
+public:
+	Transform transform;
+	
+
+};
+
+void Draw_Shrine_Init(Shrine* Shrine);
+void Draw_Shrine_Update(Shrine* Shrine);
+
+
+
+
+#endif
